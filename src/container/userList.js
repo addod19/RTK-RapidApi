6bacba5e872ec8bb53cde43b5f1d0685e93c045e
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-11-26 17:34:19 +0000

    Retrieve data from API to the UI

--- src/container/userList.js
+++ src/container/userList.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import '../assets/css/userList.css';
+
 const fetchUser = async (username) => {
   const apiConfig = {
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Methods': 'GET, POST',
+      Accept: 'application/json',
       'Access-Control-Allow-Origin': '*',
     },
   };
@@ -20,14 +22,19 @@ const fetchUser = async (username) => {
 };
 
 const UserList = () => {
-  const [user, setUser] = useState('');
-  const [username, setUserName] = useState('');
+  const [users, setUsers] = useState('');
+  const [username, setUserName] = useState('addodaniellarbi');
 
-  useEffect(() => {
-    fetchUser('addodaniellarbi').then((user) => {
-      setUser(user || '');
-    });
-  }, []);
+  let content = null;
+
+  useEffect(async () => {
+    // fetchUser('addodaniellarbi').then((users) => {
+    //   setUsers(users || '');
+    // });
+    const result = await axios.get(`https://torre.bio/api/bios/${username}`);
+    console.log(result);
+    setUsers(result.data);
+  }, [username]);
 
   const handleChange = (e) => {
     setUserName(e.target.value);
@@ -36,12 +43,38 @@ const UserList = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(`submitted ${username}`);
+    fetchUser();
   };
+
+  if (users) {
+    // const [strengths] = users.person;
+    content = (
+      <div className="main-wrapper">
+        <div className="user-profile">
+          <img src={users.person.pictureThumbnail} alt="some user" />
+          <p>{users.person.name}</p>
+        </div>
+
+        <div className="skills-interset">
+          <div className="master-influencer">
+            <p>{users.strengths[27].name}</p>
+            <p>{users.strengths[28].name}</p>
+          </div>
+          <div className="proficient">
+            <p>{users.strengths[26].name}</p>
+            <p>{users.strengths[17].name}</p>
+            <p>{users.strengths[0].name}</p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    <p>LOading.........</p>;
+  }
 
   return (
     <>
       {/* map and display the users here */}
-      {user}
       <input
         type="text"
         placeholder="Enter your torre username"
@@ -50,6 +83,7 @@ const UserList = () => {
         onChange={handleChange}
       />
       <button type="submit" onSubmit={handleSubmit}>Get User</button>
+      {content}
     </>
   );
 };
