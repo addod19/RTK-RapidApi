bae2d9368bcc8acc468eb144d215a507a8807686
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-01-13 15:15:07 +0000

    Extract and display api data (stats) object

--- cryptoa/src/components/HomePage.jsx
+++ cryptoa/src/components/HomePage.jsx
@@ -10,16 +10,20 @@ const { Title } = Typography;
 const HomePage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
 
-  console.log(data);
+  const globalState = data?.data?.stats;
+
+
+
+  if (isFetching) return "Loading!!!!";
   return (
     <>
       <Title level={2} className='heading'>Global Crypto Stats</Title>
       <Row>
-        <Col span={12}><Statistic title="Total CryptoCUrrencies" value="5"></Statistic></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value="5"></Statistic></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value="5"></Statistic></Col>
-        <Col span={12}><Statistic title="Total 24hour VOLUMN" value="5"></Statistic></Col>
-        <Col span={12}><Statistic title="Total MARKETS" value="5"></Statistic></Col>
+        <Col span={12}><Statistic title="Total CryptoCUrrencies" value={globalState.total}></Statistic></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalState.totalExchanges)}></Statistic></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalState.totalMarketCap)}></Statistic></Col>
+        <Col span={12}><Statistic title="Total 24hour VOLUMN" value={millify(globalState.total24hVolume)}></Statistic></Col>
+        <Col span={12}><Statistic title="Total MARKETS" value={millify(globalState.totalMarkets)}></Statistic></Col>
       </Row>
     </>
   )
