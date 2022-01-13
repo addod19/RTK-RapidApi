085af11fd3aac64899bae117afcba54bb1db3aaa
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-01-13 12:48:38 +0000

    Added a few components for layouts

--- cryptoa/src/App.js
+++ cryptoa/src/App.js
@@ -1,4 +1,8 @@
-import { Navbar } from "./components";
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Exchanges, HomePage, CryptoCurrencies, CryptoDetails, News } from "./components";
 
 function App() {
   return (
@@ -7,7 +11,27 @@ function App() {
         <Navbar />
       </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrentcies">
+                <CryptoCurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className="footer">
 
---
+++ cryptoa/src/components/CryptoCurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoCurrencies = () => {
+  return (
+    <div>
+      <h1>Crypto</h1>    
+    </div>
+  )
+}
+
+export default CryptoCurrencies;
