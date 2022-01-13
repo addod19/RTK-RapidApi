0eacf1d4490f098cd4eeafe9b95f7c7e84d4e01e
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-01-13 14:18:47 +0000

    Implement React-Redux and Redux toolkit and connect with API

--- cryptoa/src/components/HomePage.jsx
+++ cryptoa/src/components/HomePage.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const { Title } = Typography;
 
 const HomePage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+
+  console.log(data);
   return (
-    <div>
-      <h1>Home Page</h1>    
-    </div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title="Total CryptoCUrrencies" value="5"></Statistic></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value="5"></Statistic></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value="5"></Statistic></Col>
+        <Col span={12}><Statistic title="Total 24hour VOLUMN" value="5"></Statistic></Col>
+        <Col span={12}><Statistic title="Total MARKETS" value="5"></Statistic></Col>
+      </Row>
+    </>
   )
 }
 
---
+++ cryptoa/src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': '799b9cc14emsh9f25bfec0439608p176b8cjsn760190b3e59c'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins'),
+    })
+  })
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
