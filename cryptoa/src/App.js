2b6126e6a565be6896d5358ab4c1be906d07c13c
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-01-13 13:06:57 +0000

    Improve styling with ant design css

--- cryptoa/src/App.js
+++ cryptoa/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import { Navbar, Exchanges, HomePage, CryptoCurrencies, CryptoDetails, News } from "./components";
@@ -13,23 +13,13 @@ function App() {
       <div className="main">
         <Layout>
           <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-              <Route exact path="/exchanges">
-                <Exchanges />
-              </Route>
-              <Route exact path="/cryptocurrentcies">
-                <CryptoCurrencies />
-              </Route>
-              <Route exact path="/crypto/:coinId">
-                <CryptoDetails />
-              </Route>
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<CryptoCurrencies />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
           </div>
         </Layout>
       </div>
